b1311447c5a9b99a23c5e6da79ecce270957fc77
Richard Bell <rcbell@eng.ucsd.edu>
2019-11-17 18:49:54 -0800

    added contours corresponding to time in nanoseconds to plots

--- matlab/scripts/plot_single_emitter.m
+++ matlab/scripts/plot_single_emitter.m
@@ -1,14 +1,15 @@
 clear; close all
 addpath('../functions')
 
-show_plots = 1;         % show plots for debugging
+show_plots = 0;         % show plots for debugging
 show_circles = 1;       % plot circles centered on emitter to visualize tdoa
 show_hyperbolas = 1;    % plot hyperbolas to visualize intersection point
-targetPos = [-35; 100];     % target position (meters)
-refPos = [-50 50 0 ; ... % reference receiver positions [x; y] (meters)
-          -50 -50 50 ];  
+targetPos = [-7; 0];     % target position (meters)
+refPos = [-5 5 0 ; ... % reference receiver positions [x; y] (meters)
+          -5 -5 5 ];  
 % refPos = [0  -40  40 -70 70; ... % 5-pnt star
 %           80 -40 -40  40 40];
+bounds = [-15 15 -15 15];
 tx_pwr_dbm = 100;       % emitter transmit power in dBm
 Ntrials = 1;            % number of simulations per emitter location
 fs = 20e6;              % receiver sample rates (Hz)
@@ -22,8 +23,6 @@ wlen = 2*sps+1;         % moving maximum window length in samples
 nstds = 6;              % number of standard deviations to declare peak
 c = 299792458;          % speed of light m/s
 
-bounds = [-150 150 -150 150];
-
 [coords, bias_coords, covar_coords, mse_coords, tdoas] = ...
     get_single_emitter(targetPos, refPos, Ntrials, tx_pwr_dbm, fc, ...
     fs, Nsym, span, sps, fhigh, wlen, nstds, show_plots);
@@ -33,11 +32,17 @@ figure
 subplot(1,2,1)
 numrefs = size(refPos,2);
 for ii = 1:numrefs
-    plot(refPos(1,ii), refPos(2,ii), 'rx','HandleVisibility','off', 'MarkerSize',14); 
+    plot(refPos(1,ii), refPos(2,ii), 'ks', 'MarkerFaceColor', 'k', ...
+        'MarkerSize',10, 'HandleVisibility','off'); 
     hold all
-    text(refPos(1,ii), refPos(2,ii), sprintf('%i', ii), 'horizontalalignment', 'center', 'verticalalignment', 'bottom')
 end
-plot(refPos(1,1), refPos(2,1), 'rx')
+plot(refPos(1,1), refPos(2,1), 'ks', 'MarkerFaceColor', 'k')
+for ii = 1:numrefs
+    h = text(refPos(1,ii), refPos(2,ii), sprintf('%i', ii), ...
+        'horizontalalignment', 'center', 'verticalalignment', 'middle',...
+        'FontSize', 8);
+    set(h, 'Color',[1, 1 ,1])
+end
 plot(targetPos(1),targetPos(2), 'bo', 'MarkerSize',8);
 for ii = 1:size(coords,2)
     plot(coords(1,ii),coords(2,ii), 'b.', 'MarkerSize',12, 'HandleVisibility','off');
@@ -49,7 +54,15 @@ xlabel('x (m)')
 ylabel('y (m)')
 title('Localization Results')
 legend('Receiver Locations', 'Target Emitter Location', 'Estimated Target Location', 'location', 'best')
-grid on
+% grid on
+
+% Plot time contours
+tbounds = 2*bounds;
+[tx,ty] = meshgrid(tbounds(1):tbounds(2),tbounds(3):tbounds(4));
+tx2 = tx - targetPos(1);
+ty2 = ty - targetPos(2);
+tz = sqrt(tx2.^2+ty2.^2)/c*1e9;
+contour(tx,ty,tz, '--', 'showtext', 'on', 'HandleVisibility', 'off')
 
 subplot(1,2,2)
 for ii = 1:numrefs
@@ -79,4 +92,8 @@ end
 if show_hyperbolas == 1
     plot_multiple_hyperbolas(refPos, targetPos, tdoas*c, bounds)
 end
+
+
+
+
 
